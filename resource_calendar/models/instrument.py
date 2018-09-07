87646d9207b1c830cb5c36c8a3b7b1fbe95ab30d
Luis <luis.garcia@savoirfairelinux.com>
2018-09-07 10:05:08 -0400

    [ADD] instrument

--- resource_calendar/models/instrument.py
+++ resource_calendar/models/instrument.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, api, fields, models
+from odoo import fields, models
+
 
 class Instrument(models.Model):
     _inherit = ['resource.resource']
@@ -11,12 +12,24 @@ class Instrument(models.Model):
         string='Name',
     )
     room_id = fields.Many2one(
-        string='Room',
-        comodel_name='resource_calendar.room',
-        inverse_name='name'
-    )
+        'resource_calendar.room',
+        string='Room',)
     resource_id = fields.One2many(
         string='Resource Id',
         comodel_name='resource.resource',
         inverse_name='name',
     )
+    sku = fields.Char(
+        string='Sku',
+    )
+    type_id = fields.Many2one(
+        string=u'type_id',
+        comodel_name='model.name',
+        ondelete='set null',
+    )
+    item_type = fields.Selection([
+        ('equipment', 'Equipment'),
+        ('instruments', 'Instruments'),
+        ('services', 'Services'),
+        ('consumables', 'Consumables'),
+        ], string='Type', index=True, copy=False, default='equipment', track_visibility='onchange')
--- resource_calendar/models/room_type.py
+++ resource_calendar/models/room_type.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, api, fields, models
+from odoo import fields, models
+
 
 class RoomType(models.Model):
     _name = 'resource_calendar.room_type'
